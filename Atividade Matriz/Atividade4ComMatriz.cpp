530552de04a5130561148aa749e0e54676fd2e3b
TiagoPassos9 <102630544+TiagoPassos9@users.noreply.github.com>
2023-06-30 19:46:12 -0300

    Update Atividade4ComMatriz.cpp

--- Atividade Matriz/Atividade4ComMatriz.cpp
+++ Atividade Matriz/Atividade4ComMatriz.cpp
@@ -1,26 +1,26 @@
 #include <stdio.h>
 #include <locale.h>
 
-//Faça um programa que permita ao usuário entrar com uma matriz de tamanho 3 × 3 de números inteiros. Em seguida, calcule um vetor contendo três posições,
-//em que cada posição deverá armazenar a soma dos números de cada coluna da matriz.
+//FaÃ§a um programa que permita ao usuÃ¡rio entrar com uma matriz de tamanho 3 Ã— 3 de nÃºmeros inteiros. Em seguida, calcule um vetor contendo trÃªs posiÃ§Ãµes,
+//em que cada posiÃ§Ã£o deverÃ¡ armazenar a soma dos nÃºmeros de cada coluna da matriz.
 //Exiba na tela esse array. Por exemplo, a matriz
 //													[5 -8 10]
 //													[1  2 15]
 //													[25 10 7]
-//deverá gerar o vetor
+//deverÃ¡ gerar o vetor
 //												  	[31 4 32]
 
 int input, j, k;
 int vet[3][3], finalVet[3], *ptr;
 
-//Criando função para realizar a soma da matriz e transformar em um vetor.
+//Criando funÃ§Ã£o para realizar a soma da matriz e transformar em um vetor.
 void calculaVetores(int v[3][3], int v2[]) {
-	//Criando e inicializando as variáveis
+	//Criando e inicializando as variÃ¡veis
 	int valor1 = 0;
 	int valor2 = 0;
 	int valor3 = 0;
 
-	//Percorre cada valor da matriz, se ele estiver na primeira coluna ele irá somar no valor1, se tiver na segunda coluna irá somar no valor2, se tiver na terceira coluna irá somar no valor3.
+	//Percorre cada valor da matriz, se ele estiver na primeira coluna ele irÃ¡ somar no valor1, se tiver na segunda coluna irÃ¡ somar no valor2, se tiver na terceira coluna irÃ¡ somar no valor3.
 	//O primeiro for percorre as linhas, e o segundo percorre as colunas.
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
@@ -33,7 +33,7 @@ void calculaVetores(int v[3][3], int v2[]) {
 			}
 		}
 	}
-	//Passando o valor das colunas somadas para o vetor final que será mostrado.
+	//Passando o valor das colunas somadas para o vetor final que serÃ¡ mostrado.
 	finalVet[0] = valor1;
 	finalVet[1] = valor2;
 	finalVet[2] = valor3;
@@ -44,7 +44,7 @@ int main() {
 
 	printf("Calculando as colunas de uma matriz.\n\n");
 
-	//Colocando o ponteiro no vetor vet para facilitar pegar os números no scanf.
+	//Colocando o ponteiro no vetor vet para facilitar pegar os nÃºmeros no scanf.
     ptr = &vet[0][0];
     j = 0;
     k = 1;
@@ -52,9 +52,10 @@ int main() {
 	//Percorrendo o vetor vet com o ponteiro para pegar a matriz a ser somada.
 	for (int i = 0; i < 9; i++) {
     	j++;
-    	printf("Digite o número da linha %d coluna %d: ", k, j);
+    	printf("Digite o nÃºmero da linha %d coluna %d: ", k, j);
     	scanf("%d", ptr);
     	ptr++;
+		//Limpando buffer
 		fflush(stdin);
 
     	if (j==3) {
@@ -72,7 +73,8 @@ int main() {
 		printf("\n");
 	}
 
-	//Chamando a função calculaVetores() que está na linha 11 desse código.
+	printf("\n");
+	//Chamando a funÃ§Ã£o calculaVetores() que estÃ¡ na linha 11 desse cÃ³digo.
 	calculaVetores(vet, finalVet);
 
 	//Mostrando na tela a matriz somada.
